35c7fb17b38aae4140a9c315678aca5157cc23df
Mohan Raj <mohanmwraj@gmail.com>
2025-09-29 12:49:52 +0530

    Added Controller

---
+++ src/main/java/com/mohan/banking_app/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.mohan.banking_app.controller;
+
+import com.mohan.banking_app.dto.AccountDto;
+import com.mohan.banking_app.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // Add Account REST API
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+}
