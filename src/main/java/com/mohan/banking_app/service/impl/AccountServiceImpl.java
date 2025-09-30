da21b3ffc5c7aa89a52ef82777f707e829445126
Mohan Raj <mohanmwraj@gmail.com>
2025-09-30 10:18:59 +0530

    PUT Deposit REST API Added

--- src/main/java/com/mohan/banking_app/service/impl/AccountServiceImpl.java
+++ src/main/java/com/mohan/banking_app/service/impl/AccountServiceImpl.java
@@ -30,4 +30,18 @@ public class AccountServiceImpl implements AccountService {
                                .orElseThrow(() -> new RuntimeException("Account does not exists"));
         return AccountMapper.mapToAccountDto(account);
     }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exists"));
+
+        double total  = account.getBalance() + amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
 }
